6373ea711d47d0488f5433ce843bd0970c98490c
akthernahela <nahela00@gmail.com>
2025-10-17 14:53:18 +0200

    Creat an objects array

--- snack_1/main.js
+++ snack_1/main.js
@@ -4,3 +4,13 @@ Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore.
 */
+
+const bickes = [
+
+    {
+        id: 1,
+        name: 'Mountain bike',
+        weight: 9,
+    },
+
+]
