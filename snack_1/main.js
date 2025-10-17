86417fccc844b4a9bb3757d59ba2e9df7a6867b1
akthernahela <nahela00@gmail.com>
2025-10-17 15:01:21 +0200

    Add 4 types of bike and their weight

--- snack_1/main.js
+++ snack_1/main.js
@@ -6,11 +6,24 @@ Stampare a schermo la bici con peso minore.
 */
 
 const bickes = [
-
     {
         id: 1,
         name: 'Mountain bike',
-        weight: 9,
+        weight: 16,
     },
-
+    {
+        id: 2,
+        name: 'City bike',
+        weight: 12,
+    },
+    {
+        id: 3,
+        name: 'Gravel bike',
+        weight: 10,
+    },
+    {
+        id: 4,
+        name: 'Fat bike',
+        weight: 20,
+    },
 ]
